f39deafc06940c5f39aeb1314dc68a10b84872ed
MohammoD2 <143773181+MohammoD2@users.noreply.github.com>
2024-08-13 21:37:07 +0600

    Initialize DVC and add raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='For the shortest of time and data , i will do this project with the help of gurgoun city india',
+    author='Mohammod Ibrahim Hossain',
+    license='',
+)
