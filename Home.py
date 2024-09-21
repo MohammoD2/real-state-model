96cc068ccfa5e2737b944b6d951d9d21f703e4a7
Mohammod Ibrahim <143773181+MohammoD2@users.noreply.github.com>
2024-09-21 22:50:23 +0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,9 +2,8 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Gurgaon Real Estate Analytics App",
-    page_icon="👋",
+    page_icon="🏘️",
 )
-
-st.write("# Welcome to Streamlit! 👋")
-
+st.header("Welcome to my real estate project🏘️")
+st.image("R.jpg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 st.sidebar.success("Select a demo above.")
